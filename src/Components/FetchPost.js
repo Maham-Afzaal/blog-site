4606bc198830070448fb83e71f8e7f7ee9bf090c
Maham-Afzaal <124619654+Maham-Afzaal@users.noreply.github.com>
2023-07-27 21:55:13 +0100

    updating the file

--- src/Components/FetchPost.js
+++ src/Components/FetchPost.js
@@ -3,7 +3,8 @@ import React from 'react'
 const FetchPost = ({post}) => {
   return (
     <div>
-      <h2>{post.title} hyyyyyyyyy</h2>
+      <h2>{post.title}</h2>
+      <p>Blog Post</p>
       <p>{post.body}</p>
                 
                 
